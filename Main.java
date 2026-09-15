9abd9616032caabce45cd92652a84f036f38f54d
Kavishka-santhush <104711168+Kavishka-santhush@users.noreply.github.com>
2026-09-15 19:17:03 +0530

    gfdg

--- Main.java
+++ Main.java
@@ -1,22 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Program started...");
+        // 1. ArrayList Example (Storing a list of car brands)
+        ArrayList<String> carBrands = new ArrayList<>();
+        
+        carBrands.add("Toyota");
+        carBrands.add("Honda");
+        carBrands.add("Tesla");
 
-        int a = 10;
-        int b = 0;
-
-        try {
-            // Trying to divide by zero (Will throw ArithmeticException)
-            int result = a / b;
-            System.out.println("Result: " + result);
-        } catch (ArithmeticException e) {
-            // Handling the exception gracefully
-            System.out.println("Error caught: Cannot divide a number by zero!");
-        } finally {
-            // This block will always execute regardless of an exception
-            System.out.println("Cleanup actions done in finally block.");
+        System.out.println("--- Car Brands List ---");
+        for (String brand : carBrands) {
+            System.out.println(brand);
         }
 
-        System.out.println("Program continues running safely without crashing!");
+        // 2. HashMap Example (Storing car brand and its model count as Key-Value)
+        HashMap<String, Integer> carModelsCount = new HashMap<>();
+        
+        carModelsCount.put("Toyota", 15);
+        carModelsCount.put("Honda", 10);
+        carModelsCount.put("Tesla", 5);
+
+        System.out.println("\n--- Car Models Count (HashMap) ---");
+        System.out.println("Toyota models available: " + carModelsCount.get("Toyota"));
+        System.out.println("Tesla models available: " + carModelsCount.get("Tesla"));
     }
 }
