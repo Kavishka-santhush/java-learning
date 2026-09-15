e3ceaec91437cf190a5ef9c3cf9451ae7ea3c32a
Kavishka-santhush <104711168+Kavishka-santhush@users.noreply.github.com>
2026-09-15 19:28:02 +0530

    regre

--- Main.java
+++ Main.java
@@ -1,26 +1,44 @@
-// Generic Class with type parameter 'T'
-class Box<T> {
-    private T item;
+// 1. Defining an Enum for Car Status
+enum CarStatus {
+    AVAILABLE, RUNNING, UNDER_MAINTENANCE
+}
 
-    public void setItem(T item) {
-        this.item = item;
+// 2. Singleton Class (Only one instance can be created)
+class DatabaseConnection {
+    // Private static instance
+    private static DatabaseConnection instance;
+
+    // Private constructor so no one can use 'new DatabaseConnection()' outside
+    private DatabaseConnection() {
+        System.out.println("Database Connection Established!");
     }
 
-    public T getItem() {
-        return item;
+    // Public method to get the single instance
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public void query(String sql) {
+        System.out.println("Executing query: " + sql);
     }
 }
 
 public class Main {
-    public static void main(String[] args) { 
-        // 1. Creating a Box for Strings
-        Box<String> stringBox = new Box<>();
-        stringBox.setItem("Electric Car Battery");
-        System.out.println("String Box contains: " + stringBox.getItem());
+    public static void main(String[] args) {
+        // Testing Singleton
+        DatabaseConnection db1 = DatabaseConnection.getInstance();
+        db1.query("SELECT * FROM cars");
 
-        // 2. Creating a Box for Integers
-        Box<Integer> intBox = new Box<>();
-        intBox.setItem(5000);
-        System.out.println("Integer Box contains: " + intBox.getItem());
+        DatabaseConnection db2 = DatabaseConnection.getInstance();
+        
+        // Checking if both references point to the exact same object in memory
+        System.out.println("Are both db1 and db2 the same instance? " + (db1 == db2));
+
+        // Testing Enum
+        CarStatus status = CarStatus.RUNNING;
+        System.out.println("Current Car Status: " + status);
     }
 }
