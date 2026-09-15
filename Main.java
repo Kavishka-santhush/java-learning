71cfe668979704348e09fb8cde81978d2000309f
Kavishka-santhush <104711168+Kavishka-santhush@users.noreply.github.com>
2026-09-15 19:46:24 +0530

    ergre

--- Main.java
+++ Main.java
@@ -1,37 +1,49 @@
-// Implementing Runnable interface for a task
-class CarEngineWorker implements Runnable {
-    private String carName;
+// 1. Common Car Interface
+interface Car {
+    void assemble();
+}
 
-    public CarEngineWorker(String carName) {
-        this.carName = carName;
-    }
-
+// 2. Concrete Implementations
+class ElectricCar implements Car {
     @Override
-    public void run() {
-        for (int i = 1; i <= 3; i++) {
-            System.out.println(carName + " engine checking step: " + i);
-            try {
-                Thread.sleep(500); // Pause for 500 milliseconds
-            } catch (InterruptedException e) {
-                System.out.println("Thread interrupted.");
-            }
+    public void assemble() {
+        System.out.println("Assembling an Electric Car with battery and motors.");
+    }
+}
+
+class GasCar implements Car {
+    @Override
+    public void assemble() {
+        System.out.println("Assembling a Gas Car with engine and fuel tank.");
+    }
+}
+
+// 3. Car Factory Class (Handles object creation logic)
+class CarFactory {
+    public static Car createCar(String type) {
+        if (type == null) {
+            return null;
         }
-        System.out.println(carName + " check completed!");
+        if (type.equalsIgnoreCase("ELECTRIC")) {
+            return new ElectricCar();
+        } else if (type.equalsIgnoreCase("GAS")) {
+            return new GasCar();
+        }
+        return null;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main thread started.");
+        // Using Factory to create objects without using 'new' directly in main
+        Car car1 = CarFactory.createCar("ELECTRIC");
+        if (car1 != null) {
+            car1.assemble();
+        }
 
-        // Creating two threads for two cars
-        Thread t1 = new Thread(new CarEngineWorker("Tesla"));
-        Thread t2 = new Thread(new CarEngineWorker("BMW"));
-
-        // Starting threads (Runs concurrently)
-        t1.start();
-        t2.start();
-
-        System.out.println("Main thread finished initiating workers.");
+        Car car2 = CarFactory.createCar("GAS");
+        if (car2 != null) {
+            car2.assemble();
+        }
     }
 }
