faf7790892db6e46aa71d06a5703f70935f680c6
Kavishka-santhush <104711168+Kavishka-santhush@users.noreply.github.com>
2026-09-15 17:11:37 +0530

    ergreg

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
+// Blueprint (Class)
+class Car {
+    // Attributes (State)
+    String brand;
+    int year;
+
+    // Method (Behavior)
+    public void displayInfo() {
+        System.out.println("Car Brand: " + brand + ", Year: " + year);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("මම VS Code එකෙන් Java ඉගෙන ගන්න පටන් ගත්තා!");
+        // Creating Object 1 (Stored in Heap, reference in Stack)
+        Car car1 = new Car();
+        car1.brand = "Toyota";
+        car1.year = 2022;
+
+        // Creating Object 2
+        Car car2 = new Car();
+        car2.brand = "Honda";
+        car2.year = 2024;
+
+        // Calling methods
+        car1.displayInfo();
+        car2.displayInfo();
     }
 }
