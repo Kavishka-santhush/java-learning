3fef943bf404ebafae4c06a448d3ccf4be3d7d40
Kavishka-santhush <104711168+Kavishka-santhush@users.noreply.github.com>
2026-09-15 19:33:48 +0530

    fdgfdg

--- Main.java
+++ Main.java
@@ -1,21 +1,57 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+// 1. SRP: Car class only holds car properties (Single Responsibility)
+class Car {
+    private String brand;
+    private int year;
+
+    public Car(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    public String getBrand() { return brand; }
+    public int getYear() { return year; }
+}
+
+// Separate class responsible only for database operations (SRP)
+class CarRepository {
+    public void saveToDatabase(Car car) {
+        System.out.println("Saving " + car.getBrand() + " to the database...");
+    }
+}
+
+// 2. OCP: Printing interface/class which is open for extension
+interface CarPrinter {
+    void print(Car car);
+}
+
+class ConsolePrinter implements CarPrinter {
+    @Override
+    public void print(Car car) {
+        System.out.println("Printing Car Details -> Brand: " + car.getBrand() + ", Year: " + car.getYear());
+    }
+}
+
+// If we want a JSON printer tomorrow, we just implement CarPrinter without modifying existing code (OCP)
+class JsonPrinter implements CarPrinter {
+    @Override
+    public void print(Car car) {
+        System.out.println("{\"brand\": \"" + car.getBrand() + "\", \"year\": " + car.getYear() + "}");
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        // Creating a list of car brands
-        List<String> carBrands = Arrays.asList("Toyota", "Tesla", "Honda", "Tata", "Tesla Model S");
+        Car myCar = new Car("Tesla", 2026);
 
-        System.out.println("--- Filtering cars starting with 'T' using Streams ---");
+        // Saving using repository (SRP)
+        CarRepository repo = new CarRepository();
+        repo.saveToDatabase(myCar);
 
-        // Using Stream API and Lambda Expression
-        List<String> filteredCars = carBrands.stream()
-                .filter(car -> car.startsWith("T")) // Lambda expression to filter
-                .map(String::toUpperCase)           // Converting to uppercase
-                .collect(Collectors.toList());      // Collecting results into a list
+        // Printing using polymorphism and OCP
+        CarPrinter printer1 = new ConsolePrinter();
+        printer1.print(myCar);
 
-        // Printing the filtered list
-        filteredCars.forEach(car -> System.out.println(car));
+        CarPrinter printer2 = new JsonPrinter();
+        printer2.print(myCar);
     }
 }
