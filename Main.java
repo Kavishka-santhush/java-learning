cda6151bc2c5b6e23e69d82f85fc1517d8562e0a
Kavishka-santhush <104711168+Kavishka-santhush@users.noreply.github.com>
2026-09-15 19:29:55 +0530

    reger

--- Main.java
+++ Main.java
@@ -1,44 +1,21 @@
-// 1. Defining an Enum for Car Status
-enum CarStatus {
-    AVAILABLE, RUNNING, UNDER_MAINTENANCE
-}
-
-// 2. Singleton Class (Only one instance can be created)
-class DatabaseConnection {
-    // Private static instance
-    private static DatabaseConnection instance;
-
-    // Private constructor so no one can use 'new DatabaseConnection()' outside
-    private DatabaseConnection() {
-        System.out.println("Database Connection Established!");
-    }
-
-    // Public method to get the single instance
-    public static DatabaseConnection getInstance() {
-        if (instance == null) {
-            instance = new DatabaseConnection();
-        }
-        return instance;
-    }
-
-    public void query(String sql) {
-        System.out.println("Executing query: " + sql);
-    }
-}
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        // Testing Singleton
-        DatabaseConnection db1 = DatabaseConnection.getInstance();
-        db1.query("SELECT * FROM cars");
+        // Creating a list of car brands
+        List<String> carBrands = Arrays.asList("Toyota", "Tesla", "Honda", "Tata", "Tesla Model S");
 
-        DatabaseConnection db2 = DatabaseConnection.getInstance();
-        
-        // Checking if both references point to the exact same object in memory
-        System.out.println("Are both db1 and db2 the same instance? " + (db1 == db2));
+        System.out.println("--- Filtering cars starting with 'T' using Streams ---");
 
-        // Testing Enum
-        CarStatus status = CarStatus.RUNNING;
-        System.out.println("Current Car Status: " + status);
+        // Using Stream API and Lambda Expression
+        List<String> filteredCars = carBrands.stream()
+                .filter(car -> car.startsWith("T")) // Lambda expression to filter
+                .map(String::toUpperCase)           // Converting to uppercase
+                .collect(Collectors.toList());      // Collecting results into a list
+
+        // Printing the filtered list
+        filteredCars.forEach(car -> System.out.println(car));
     }
 }
